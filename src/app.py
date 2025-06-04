c96f9c4d609c3a79a02faaa43e1f2bf0e1cddb78
Liam Blignaut <liam.blignaut@rain.co.za>
2025-06-04 18:34:36 +0200

    username fix

--- src/app.py
+++ src/app.py
@@ -11,7 +11,7 @@ def details():
         {
             "time": datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y"),
             "hostname": socket.gethostname(),
-            "message": "You are doing super duper great, human!",
+            "message": "You are doing super duper whooper great, human!",
         }
     )
 

a0df1c94e84659d1f04d2bc9dd0d401de8d23a39
Liam Blignaut <liam.blignaut@rain.co.za>
2025-06-04 15:45:51 +0200

    Add ci github workflow

--- src/app.py
+++ src/app.py
@@ -11,6 +11,7 @@ def details():
         {
             "time": datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y"),
             "hostname": socket.gethostname(),
+            "message": "You are doing great, human!",
         }
     )
 

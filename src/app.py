61ac2f8fe6c0eded47448556afbfb6c5f1e5dc11
Liam Blignaut <liam.blignaut@rain.co.za>
2025-06-04 20:02:47 +0200

    shorten tag this time for sure

--- src/app.py
+++ src/app.py
@@ -11,7 +11,7 @@ def details():
         {
             "time": datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y"),
             "hostname": socket.gethostname(),
-            "message": "You are doing super duper whooper pooper snooper gooper chooper great, human!",
+            "message": "You are doing super duper whooper pooper snooper gooper chooper flooper great, human!",
         }
     )
 
